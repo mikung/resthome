91fab4420e06c664387e11a63a3c0df03743648a
Mikung <mikung555@gmail.com>
2018-10-10 12:32:33 +0700

    update gitignore

--- pages/main/indexadmin.php
+++ pages/main/indexadmin.php
@@ -5,7 +5,7 @@
         <!-- Content Header (Page header) -->
         <section class="content-header">
             <h1>
-                จัดการบ้านพักออนไลน์ test test2 louis
+                จัดการบ้านพักออนไลน์
                 <small>(<?php $classTM->departName($_SESSION["departID"]); ?>)</small>
             </h1>
             <ol class="breadcrumb">
